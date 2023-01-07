c099b034dbe828f6e1e790684f770b07de87e01c
dork <38471874+qt-dork@users.noreply.github.com>
2023-01-06 19:43:20 -0600

    fixed skeleton layout <3

---
+++ src/config.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Society for Internet Blaseball Research';
+export const SITE_DESCRIPTION = 'A site for the study of the game of Blaseball.';
