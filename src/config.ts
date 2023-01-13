5043c90da708dfde3aae42e154902b64667d4c32
dork <38471874+qt-dork@users.noreply.github.com>
2023-01-12 21:25:05 -0600

    open graph deez nuts

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Society for Internet Blaseball Research';
-export const SITE_DESCRIPTION = 'A site for the study of the game of Blaseball.';
+export const SITE_DESCRIPTION = 'The Society for Internet Blaseball Research is a community devoted to unravelling Blaseball’s deepest mysteries.';
